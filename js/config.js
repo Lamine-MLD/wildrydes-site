d026f50f6ea48fa86df02c136ea2327dbc977d21
Lamine-MLD <mohamedlaminediabyaws1@gmail.com>
2025-07-23 11:54:07 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_RYH7qqOfm', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6fpf696f3217ue978k9f8a6aku', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
